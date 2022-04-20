ef2a5c3db023c34c92004416805900b5b660f891
mpunica <monika.gordziejewska@gmail.com>
2022-04-20 18:58:22 +0200

    pytest 1.0

--- weddingapp/tests.py
+++ weddingapp/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import pytest
 
-# Create your tests here.
+def test_main_page():
+    c = Client()
+    response = c.get('/')
+    assert response.status_code == 200
+    assert "Bride&Groom - WeddingApp" in str(response.content, "utf-8")
+
+def test_login_view():
+    c = Client()
+    response = c.get('/login/')
+    assert response.status_code == 200
